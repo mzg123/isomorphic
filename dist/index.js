82906d91fc63a6346d294de23225154d6d9dfbf4
zhigao miao <mzg@zhigaodeMacBook-Pro.local>
2018-02-04 20:01:46 +0800

    启动hapi

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,21 @@
-import Hapi from 'hapi';
-const server = new Hapi.Server();
-server.connection({
-    host: 'localhost',
-    port: 8000
-});
+'use strict';
 
+var _hapi = require('hapi');
+
+var _hapi2 = _interopRequireDefault(_hapi);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var server = _hapi2.default.Server({
+	host: 'localhost',
+	port: 8000
+});
 server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply) {
-        reply('hello word');
-    }
+	method: 'GET',
+	path: '/hello',
+	handler: function handler(request, reply) {
+		return "hello work";
+	}
 });
 
 server.start();
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import Hapi from 'hapi';
-const server = new Hapi.Server();
-server.connection({
-    host: 'localhost',
-    port: 8000
+const server =  Hapi.Server({
+	host: 'localhost',
+	port: 8000
 });
-
-
-server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply){
-        reply('hello word');
-    }
-});
+	server.route({
+	    method: 'GET',
+	    path: '/hello',
+	    handler: function (request, reply){
+			return "hello work";
+	    }
+	});
 
 server.start();
